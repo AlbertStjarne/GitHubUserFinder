74994451704fa668f4ba5f91afb14296bde0ccb9
Albert Stjärne <albert.stjarne@gmail.com>
2019-02-27 13:04:46 +0100

    adds login name and fixing view profile link

--- ui.js
+++ ui.js
@@ -10,7 +10,7 @@ class UI {
       <div class="row">
         <div class="col-md-3">
           <img class="img-fluid mb-2" src="${user.avatar_url}">
-          <a href="${user.html_url} target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+          <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
         </div>
         <div class="col-md-9">
           <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
@@ -19,6 +19,7 @@ class UI {
           <span class="badge badge-primary">Following: ${user.following}</span>
           <br><br>
           <ul class="list-group">
+            <li class="list-group-item">Login: ${user.login}</li>
             <li class="list-group-item">Company: ${user.company}</li>
             <li class="list-group-item">Website/Blog: ${user.blog}</li>
             <li class="list-group-item">Location: ${user.location}</li>
